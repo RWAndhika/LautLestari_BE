d14c706b72664b21342707370f9a8eda05a19231
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-08-01 16:26:33 +0700

    feat: add user login func

--- controllers/users.py
+++ controllers/users.py
@@ -32,3 +32,25 @@ def register_user():
     
     return {'message': 'Register user success'}, 200
 
+@users_routes.route('/users/login', methods=['POST'])
+def user_login():
+    try:
+        email = request.form['email']
+        user = s.query(Users).filter(Users.email == email).first()
+
+        if user == None:
+            return {'message': 'User not found'}, 403
+
+        if not user.check_password(request.form['password']):
+            return {'message': 'Invalid password'}, 403
+        
+        login_user(user)
+        session_id = request.cookies.get('session')
+        return {
+            'session_id': session_id,
+            'message': 'Login success'
+        }, 200
+    
+    except Exception as e:
+        s.rollback()
+        return {'message': 'Fail to login'}, 500
