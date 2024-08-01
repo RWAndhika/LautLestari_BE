00c9ece22fcef23b079c5611dd391ce6bf59040d
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-08-01 18:19:21 +0700

    feat: add login manager

--- index.py
+++ index.py
@@ -2,6 +2,9 @@ from flask import Flask
 from dotenv import load_dotenv
 from controllers.users import users_routes, s
 
+from flask_login import LoginManager
+from models.users import Users
+
 import os
 
 load_dotenv()
@@ -10,6 +13,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 app.register_blueprint(users_routes)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return s.query(Users).get(int(user_id))
 
 @app.route('/')
 def hello_world():
