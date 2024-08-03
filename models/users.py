90d12fb4adba31170ae51a7a9e4553eb267a00ef
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-08-03 14:39:53 +0700

    fix: products and users relationship

--- models/users.py
+++ models/users.py
@@ -21,8 +21,8 @@ class Users(Base, UserMixin):
 
     products = relationship('Products', backref='user', cascade="all, delete-orphan")
 
-    def set_password(self, password_hash):
-        self.password_hash = bcrypt.hashpw(password_hash.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    def set_password(self, password):
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
-    def check_password(self, password_hash):
-        return bcrypt.checkpw(password_hash.encode('utf-8'), self.password_hash.encode('utf-8'))
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
