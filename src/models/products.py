3d3afe5d88e815ce9281fabb9cc2343292765e99
Hari <155520906+ImamHM04@users.noreply.github.com>
2024-08-06 16:55:08 +0700

    feat:crud product

--- src/models/products.py
+++ src/models/products.py
@@ -1,6 +1,6 @@
 from models.base import Base
 
-from sqlalchemy import Integer, String, DateTime, ForeignKey
+from sqlalchemy import Integer, String, DateTime, ForeignKey, LargeBinary, Text
 from sqlalchemy.sql import func
 from sqlalchemy.orm import mapped_column
 
@@ -9,9 +9,10 @@ class Products(Base):
 
     id = mapped_column(Integer, primary_key=True, autoincrement=True)
     user_id = mapped_column(Integer, ForeignKey('users.id'))
+    # image = mapped_column(LargeBinary, nullable=False)
     price = mapped_column(Integer, nullable=False)
     qty = mapped_column(Integer, nullable=False)
-    description = mapped_column(String(255))
+    description = mapped_column(Text)
     category = mapped_column(String(255), nullable=False)
     location = mapped_column(String(255), nullable=False)
     created_at = mapped_column(DateTime(timezone=True), server_default=func.now())
